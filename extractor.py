b1dcf040e0fae74c8da20fb8493c24d02573f734
Aaditya Sawant <aadityaaacodes@gmail.com>
2024-08-01 17:09:58 +0530

    Adding JSON file

--- extractor.py
+++ extractor.py
@@ -2,8 +2,6 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-import time
 import json
 
 json_path = 'dataStorage.json'
